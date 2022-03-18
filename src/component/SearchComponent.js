d7a0e5ad90f07847e89b0f842d824a5a37c0ba58
Peram Kharel <peram1769@gmail.com>
2022-03-18 13:03:25 +0800

    fixed search

--- src/component/SearchComponent.js
+++ src/component/SearchComponent.js
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { loadPatientThunk } from "../redux/search/actions";
 import { Accordion } from "react-bootstrap";
 
-
 const SearchBar = (props) => {
   const [searchPatients, setSearchPatients] = useState("");
   const dispatch = useDispatch();
@@ -11,70 +10,100 @@ const SearchBar = (props) => {
   useEffect(() => {
     dispatch(loadPatientThunk());
   }, []);
-  
+
+  const newA = props.searchingStore.map((patient, index) => {
+    newProps.push({ id: index, ...patient });
+  });
+  console.log(newA);
+
   const key = "hkid";
 
   const arrayUniqueByKey = [
     ...new Map(newProps.map((item) => [item[key], item])).values(),
   ];
 
-
   console.log(arrayUniqueByKey);
   return (
-    <div className='search_card'>
-      <Accordion 
-      className='m-3 pharmacy_stock_card' defaultActiveKey="0" 
-      style={{  width: 1000 }}>
+    <div className="search_card">
+      <Accordion
+        className="m-3 pharmacy_stock_card"
+        defaultActiveKey="0"
+        style={{ width: 1000 }}
+      >
         <Accordion.Item eventKey="0">
           <Accordion.Header>
-            <h5 className='setting_title mx-1'>Search Patients</h5>
-          <input 
-          className='search_input'
-          type="text" value={searchPatients} onChange={(e) => setSearchPatients(e.target.value)} placeholder="Enter to search patient"/>
+            <h5 className="setting_title mx-1">Search Patients</h5>
+            <input
+              className="search_input"
+              type="text"
+              value={searchPatients}
+              onChange={(e) => setSearchPatients(e.target.value)}
+              placeholder="Enter to search patient"
+            />
           </Accordion.Header>
-        <Accordion.Body>
-
-        <div className='search_table' style={{  width: 950 }}>
-          <br />
-          {arrayUniqueByKey ? arrayUniqueByKey.filter((val) => {
-              if (val == "" || searchPatients.trim() == "") {
-                return val;
-              } else if (
-                val.hkid.toLowerCase().includes(searchPatients.toLowerCase()) ||
-                val.f_name
-                  .toLowerCase()
-                  .includes(searchPatients.toLowerCase()) ||
-                val.l_name
-                  .toLowerCase()
-                  .includes(searchPatients.toLowerCase()) ||
-                val.email.toLowerCase().includes(searchPatients.toLowerCase())
-              ) {
-                return val;
-              }
-            }).map((eachPatient, index) => {
-              return (
-                  <div 
-                  className='patient_card'
-                  key={index}>
-                    <Accordion >
-                      <Accordion.Item eventKey={index}>
-                        <Accordion.Header><span>{eachPatient.f_name} {eachPatient.l_name}</span></Accordion.Header>
-                      <Accordion.Body className='each_patient_card'>
-                        <p>HKID: <span>{eachPatient.hkid}</span></p>
-                        <p>E-mail: <span>{eachPatient.email}</span></p>
-                        <p>Gender: <span>{eachPatient.gender}</span></p>
-                        <p>Phone Number: <span>{eachPatient.phone}</span></p>
-                        <p>Drug Allergies:<span> {eachPatient.drug_allergy}</span></p>
-                      </Accordion.Body>
-                      </Accordion.Item>
-                    </Accordion>
-                  </div>
-              )})
-              : "You dont have any patients!"}
-              </div>
-              </Accordion.Body>
-              </Accordion.Item>
-              </Accordion>
+          <Accordion.Body>
+            <div className="search_table" style={{ width: 950 }}>
+              <br />
+              {arrayUniqueByKey
+                ? arrayUniqueByKey
+                    .filter((val) => {
+                      if (val == "" || searchPatients.trim() == "") {
+                        return val;
+                      } else if (
+                        val.hkid
+                          .toLowerCase()
+                          .includes(searchPatients.toLowerCase()) ||
+                        val.f_name
+                          .toLowerCase()
+                          .includes(searchPatients.toLowerCase()) ||
+                        val.l_name
+                          .toLowerCase()
+                          .includes(searchPatients.toLowerCase()) ||
+                        val.email
+                          .toLowerCase()
+                          .includes(searchPatients.toLowerCase())
+                      ) {
+                        return val;
+                      }
+                    })
+                    .map((eachPatient, index) => {
+                      return (
+                        <div className="patient_card" key={index}>
+                          <Accordion>
+                            <Accordion.Item eventKey={index}>
+                              <Accordion.Header>
+                                <span>
+                                  {eachPatient.f_name} {eachPatient.l_name}
+                                </span>
+                              </Accordion.Header>
+                              <Accordion.Body className="each_patient_card">
+                                <p>
+                                  HKID: <span>{eachPatient.hkid}</span>
+                                </p>
+                                <p>
+                                  E-mail: <span>{eachPatient.email}</span>
+                                </p>
+                                <p>
+                                  Gender: <span>{eachPatient.gender}</span>
+                                </p>
+                                <p>
+                                  Phone Number: <span>{eachPatient.phone}</span>
+                                </p>
+                                <p>
+                                  Drug Allergies:
+                                  <span> {eachPatient.drug_allergy}</span>
+                                </p>
+                              </Accordion.Body>
+                            </Accordion.Item>
+                          </Accordion>
+                        </div>
+                      );
+                    })
+                : "You dont have any patients!"}
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 };
